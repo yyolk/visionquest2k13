ac7bed5f21fd22dc30e302af8a25083b65a0b79e
Joseph Yølk Chiocchi <joe@yolk.cc>
2013-11-17 18:36:13 -0600

    show on load

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,19 @@
 require([
-  'bootstrap'
+  'bootstrap', 'jquery'
 ],
-function ( bootstrap ) {
+function ( bootstrap, $ ) {
     'use strict';
     // jquery(".page-header").css('color', 'red');
-    start();
+    $('#dinca').collapse('show');
+    
+    $('#dincaheader').click(function(){
+    //   toggleit=!toglleit;
+    
+      $('#dinca').collapse('toggle');
+    //   $('#dincaheader').toggleClass('collapsed', toggleit);
+      
+    });
+
+    // $('#dincaheader h1').css('text-decoration', 'strike-through');
+    //start();
 });
