46ef0c6a0bc764d61c4f27cc9660e32033488c85
Joseph Yølk Chiocchi <joe@yolk.cc>
2013-11-22 14:16:16 -0600

    add stats route

---
+++ app/controllers/stats.js
@@ -0,0 +1,7 @@
+exports.index = function(req, res){
+
+  res.render('stats', {
+    title: "VISIONQUEST 2013 : Hit Count | DINCA"
+  });
+
+};
--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,9 @@ module.exports = function(app){
 
 	//home route
 	var home = require('../app/controllers/home');
+  var stats = require('../app/controllers/stats');
 	app.get('/', home.index);
+  app.get('/stats', stats.index);
   
 
 };
